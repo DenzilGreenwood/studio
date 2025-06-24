58e71f8316518e50f9e28fb2267166c40bfe2e04
Denzil Greenwood <founder@cognitiveinsight.ai>
2025-06-24 17:51:43 +0000

    The `npm install` command failed in my project. Analyze the following er

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -3,7 +3,7 @@ import type {Config} from 'tailwindcss';
 export default {
   darkMode: ['class'],
   content: [
-    './src/**/*.{js,ts,jsx,tsx,mdx}',
+    './src/**/*.{js,ts,jsx,tsx,mdx,mjs}',
   ],
   theme: {
     container: {
