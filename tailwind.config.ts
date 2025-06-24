bc0cfd17c3ef8006acfea2579fe337196f750ec2
Denzil Greenwood <founder@cognitiveinsight.ai>
2025-06-24 18:01:09 +0000

    The NextJS app server is having trouble starting. Please identify what c

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -3,7 +3,8 @@ import type {Config} from 'tailwindcss';
 export default {
   darkMode: ['class'],
   content: [
-    './src/**/*.{js,ts,jsx,tsx,mdx,mjs}',
+    './src/app/**/*.{js,ts,jsx,tsx,mdx}',
+    './src/components/**/*.{js,ts,jsx,tsx,mdx}',
   ],
   theme: {
     container: {
